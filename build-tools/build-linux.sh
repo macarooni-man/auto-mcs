cc173f4091a5c2bab046249592fe85b3a1cf46f5
Kaleb Efflandt <dabawss10@gmail.com>
2024-01-02 15:43:13 -0600

    Update to build-tools

--- build-tools/build-linux.sh
+++ build-tools/build-linux.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
 # Required for installation:
-# python3.9, python3-tkinter, python3-dev, portaudio-dev, 
+# build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev tk-dev python3-dev portaudio-dev
 
 
 # Global variables
 shopt -s expand_aliases
-alias python="/usr/bin/python3.9"
-version=$( python --version )
-errorlevel=$?
+alias python="/opt/python/3.9.18/bin/python3.9"
 venv_path="./venv"
+current=$( pwd )
+
+
+# Check for a set DISPLAY variable
+if [ ${DISPLAY:-"unset"} == "unset" ]; then
+	echo A desktop environment is required to proceed
+	exit 1
+fi
 
 
 # First, check if a valid version of Python 3.9 is installed
+version=$( python --version )
+errorlevel=$?
 if [ $errorlevel -ne 0 ]; then
-    echo Please install Python 3.9 to proceed
-    exit 1
+    echo Installing Python 3.9
+
+	cd /tmp/
+	wget https://www.python.org/ftp/python/3.9.18/Python-3.9.18.tgz
+	tar xzf Python-3.9.18.tgz
+	cd Python-3.9.18
+
+	sudo ./configure --prefix=/opt/python/3.9.18/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi --enable-shared
+	sudo make -j "$(nproc)"
+
+	sudo ./python3.9 -m test -j "$(nproc)"
+	sudo make altinstall
+	sudo rm /tmp/Python-3.9.18.tgz
+
+	sudo python -m pip install --upgrade pip setuptools wheel
+	cd $current
+
+	errorlevel=$?
+	if [ $errorlevel -ne 0 ]; then
+    	echo Something went wrong installing Python, please try again
+    	exit 1
+   	fi
 
 else
-
-	# Check for a set DISPLAY variable
-	if [ ${DISPLAY:-"unset"} == "unset" ]; then
-		echo A desktop environment is required to proceed
-    	exit 1
-	fi
-
 
 	# If Python 3.9 is installed and a DE is present, check for a virtual environment
     echo Detected $version
