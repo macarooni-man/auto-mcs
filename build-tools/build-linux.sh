1ec40c64013bcb5a8e7085ce0c41a5f48ef02eaa
Kaleb Efflandt <dabawss10@gmail.com>
2024-01-02 21:09:33 -0600

    Update build-linux.sh

--- build-tools/build-linux.sh
+++ build-tools/build-linux.sh
@@ -21,11 +21,21 @@ if [ ${DISPLAY:-"unset"} == "unset" ]; then
 fi
 
 
-
 # First, check if a valid version of Python 3.9 is installed
 version=$( $python --version )
 errorlevel=$?
 if [ $errorlevel -ne 0 ]; then
+	echo Obtaining packages to build Python from source
+
+	# Determine system package manager and install appropriate packages
+	if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache wget gcc make zlib-dev libffi-dev openssl-dev musl-dev portadio-dev
+	elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev tk-dev python3-dev portaudio19-dev
+	elif [ -x "$(command -v dnf)" ];     then sudo dnf -y groupinstall "Development Tools" && sudo dnf -y install wget gcc openssl-devel bzip2-devel libffi-devel xz-devel tk-devel portaudio-devel
+	elif [ -x "$(command -v yum)" ];  then sudo yum	-y groupinstall "Development Tools" && sudo dnf -y install wget gcc openssl-devel bzip2-devel libffi-devel xz-devel tk-devel portaudio-devel
+	elif [ -x "$(command -v pacman)" ];  then sudo pacman -S --noconfirm base-devel wget tk tcl openssl portaudio
+	else echo "Package manager not found: You must manually install the Python 3.9 source dependencies">&2; fi
+
+
     echo Installing Python 3.9
 
 	cd /tmp/
@@ -36,7 +46,7 @@ if [ $errorlevel -ne 0 ]; then
 	sudo ./configure --prefix=$python_path --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi --enable-shared LDFLAGS="-Wl,-rpath $python_path/lib"
 	sudo make -j "$(nproc)"
 
-	sudo ./python3.9 -m test -j "$(nproc)"
+	# sudo ./python3.9 -m test -j "$(nproc)"
 	sudo make altinstall
 	sudo rm /tmp/Python-3.9.18.tgz
 
@@ -44,7 +54,7 @@ if [ $errorlevel -ne 0 ]; then
 
 	errorlevel=$?
 	if [ $errorlevel -ne 0 ]; then
-    	echo Something went wrong installing Python, please try again
+    	echo Something went wrong installing Python, please try again (did you install all the packages\?)
     	exit 1
    	fi
 
