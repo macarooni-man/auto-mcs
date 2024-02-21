929bd148f49f6a28393196e319a40df81cea833b
Kaleb Efflandt <dabawss10@gmail.com>
2024-02-21 10:25:43 -0600

    Update build-linux.sh

--- build-tools/build-linux.sh
+++ build-tools/build-linux.sh
@@ -61,7 +61,7 @@ if [ $errorlevel -ne 0 ]; then
 
 	cd /tmp/
 	wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz --no-check-certificate
-	tar xzf openssl-1.1.1g.tar.gz 
+	tar xzf openssl-1.1.1g.tar.gz
 	cd openssl-1.1.1g
 
 	mkdir -p $ssl_path/lib
@@ -136,7 +136,7 @@ fi
 # Install/Upgrade packages
 echo "Installing packages"
 source $venv_path/bin/activate
-su $(logname) -c "pip install --upgrade -r ./reqs-linux.txt"
+pip install --upgrade -r ./reqs-linux.txt
 
 
 # Patch and install Kivy hook for Pyinstaller
@@ -144,7 +144,7 @@ patch() {
 	kivy_path=$1"/python3.9/site-packages/kivy/tools/packaging/pyinstaller_hooks"
 	sed 's/from PyInstaller.compat import modname_tkinter/#/' $kivy_path/__init__.py > tmp.txt && mv tmp.txt $kivy_path/__init__.py
 	sed 's/excludedimports = \[modname_tkinter, /excludedimports = [/' $kivy_path/__init__.py > tmp.txt && mv tmp.txt $kivy_path/__init__.py
-	su $(logname) -c $venv_path"/bin/python3.9 -m kivy.tools.packaging.pyinstaller_hooks hook "$kivy_path"/kivy-hook.py"
+	python -m kivy.tools.packaging.pyinstaller_hooks hook "$kivy_path/kivy-hook.py"
 }
 patch $venv_path"/lib"
 patch $venv_path"/lib64"
@@ -164,7 +164,7 @@ export KIVY_AUDIO=ffpyplayer
 cd $current
 cp $spec_file ../source
 cd ../source
-su $(logname) -c "pyinstaller "$spec_file" --upx-dir "$current"/upx/linux --clean"
+pyinstaller "$spec_file" --upx-dir "$current"/upx/linux --clean
 cd $current
 rm -rf ../source/$spec_file
 mv -f ../source/dist .
