866e6fa63217a1c6cf5ba5a53f85179de802d77d
Kaleb Efflandt <dabawss10@gmail.com>
2024-03-04 15:27:28 -0600

    Attempt 2 to fix macOS SSL issue

--- source/constants.py
+++ source/constants.py
@@ -189,8 +189,8 @@ else:
     gui_assets = os.path.join(executable_folder, 'gui-assets')
 
 
-# SSL crap on linux
-if os_name == 'linux':
+# SSL crap on unix
+if os_name != 'windows':
     os.environ['SSL_CERT_DIR'] = executable_folder
     os.environ['SSL_CERT_FILE'] = os.path.join(executable_folder, 'ca-bundle.crt')
 
