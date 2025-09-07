980c189d02d3be7b8f2a6f2338966e99bda9725f
Kaleb Efflandt <dabawss10@gmail.com>
2025-09-06 20:33:44 -0500

    Update constants.py

--- source/constants.py
+++ source/constants.py
@@ -1160,7 +1160,7 @@ rm \"{script_path}\"""")
             script.write(script_content)
             send_log('restart_update_app', f"writing to '{script_path}':\n{script_content}")
 
-        run_proc(f"chmod +x \"{script_path}\" && bash \"{script_path}\" > ~/Desktop/update.log")
+        run_proc(f"chmod +x \"{script_path}\" && bash \"{script_path}\"")
         sys.exit(0)
 
 
