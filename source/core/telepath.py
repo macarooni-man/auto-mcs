98cefcf6ac4c16c73b89f2913f5a50617b567c17
Kaleb Efflandt <dabawss10@gmail.com>
2025-09-22 14:14:54 -0500

    Update telepath.py

--- source/core/telepath.py
+++ source/core/telepath.py
@@ -1107,7 +1107,7 @@ def get_function_params(method: Callable):
         )
         for param in parameters.values()
         if param.name not in ["self", "args"]
-        and '_' not in param.name
+        and not param.name.startswith('_')
     }
 
 
