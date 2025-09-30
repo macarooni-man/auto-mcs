2c298b90273f58e454c3098c6c1e157496a20442
Kaleb Efflandt <dabawss10@gmail.com>
2025-09-30 11:49:39 -0500

    Increase failure verbosity (Docker)

--- docker/launcher.sh
+++ docker/launcher.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env sh
 set -eu
 
+error () {
+    { printf '\E[31m'; printf "$@"; printf '\E[0m'; } >&2
+    exit 1
+}
+
 WEB_PORT="${WEB_PORT:-8080}"
 WEB_USERNAME="${WEB_USERNAME:-}"
 WEB_PASSWORD="${WEB_PASSWORD:-}"
 
+CRED_EXAMPLE="  - Example: WEB_USERNAME='U\$ern4me' WEB_PASSWORD='P@s\$w0rd'"
+
 
 # Basic credentials check
 if [ -z "$WEB_USERNAME" ] || [ -z "$WEB_PASSWORD" ]; then
-  echo "ERROR: 'WEB_USERNAME' and 'WEB_PASSWORD' are required." >&2
+  echo "[ERROR] The environment variables 'WEB_USERNAME' and 'WEB_PASSWORD' are required\n\n$CRED_EXAMPLE"
   exit 64
 fi
 
 case "${WEB_USERNAME}:${WEB_PASSWORD}" in
   root:auto-mcs|admin:change-me|*:change-me|*:password|admin:admin|*:admin|'':*|*:'')
-    echo "ERROR: Default/insecure credentials are not allowed." >&2
+    error "[ERROR] Default/insecure credentials are not allowed\n\n$CRED_EXAMPLE"
     exit 64
     ;;
 esac
