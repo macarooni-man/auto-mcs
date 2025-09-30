e145d9338d2e0687caf8bc9a1d76bf22f112a51c
Kaleb Efflandt <dabawss10@gmail.com>
2025-09-30 11:59:49 -0500

    Update launcher.sh

--- docker/launcher.sh
+++ docker/launcher.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env sh
 set -eu
 
-error () {
-    { printf '\E[31m'; printf "$@"; printf '\E[0m'; } >&2
-    exit 1
-}
-
 WEB_PORT="${WEB_PORT:-8080}"
 WEB_USERNAME="${WEB_USERNAME:-}"
 WEB_PASSWORD="${WEB_PASSWORD:-}"
 
-CRED_EXAMPLE="  - Example: WEB_USERNAME='U\$ern4me' WEB_PASSWORD='P@s\$w0rd'"
+CRED_EXAMPLE="  - To configure the environment variable credentials:\n    - WEB_USERNAME='U\$ern4me'\n    - WEB_PASSWORD='P@s\$w0rd'"
 
 
 # Basic credentials check
 if [ -z "$WEB_USERNAME" ] || [ -z "$WEB_PASSWORD" ]; then
-  echo "[ERROR] The environment variables 'WEB_USERNAME' and 'WEB_PASSWORD' are required\n\n$CRED_EXAMPLE"
+  printf "[ERROR] The environment variables 'WEB_USERNAME' and 'WEB_PASSWORD' are required\n\n$CRED_EXAMPLE"
   exit 64
 fi
 
 case "${WEB_USERNAME}:${WEB_PASSWORD}" in
   root:auto-mcs|admin:change-me|*:change-me|*:password|admin:admin|*:admin|'':*|*:'')
-    error "[ERROR] Default/insecure credentials are not allowed\n\n$CRED_EXAMPLE"
+    printf "[ERROR] Default/insecure credentials are not allowed\n\n$CRED_EXAMPLE"
     exit 64
     ;;
 esac
