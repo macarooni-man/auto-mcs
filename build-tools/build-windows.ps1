e327fdfba44b0ad559408390d40d615ca6b9b5ab
Kaleb Efflandt <dabawss10@gmail.com>
2025-10-28 22:36:19 -0500

    Added custom PyInstaller bootloader

--- build-tools/build-windows.ps1
+++ build-tools/build-windows.ps1
@@ -152,13 +152,18 @@ python -m kivy.tools.packaging.pyinstaller_hooks hook "$kivy_path/kivy-hook.py"
 # Rebuild locales.json
 # python locale-gen.py
 
+# Path to custom PyInstaller bootloader
+$BootloaderPath = Join-Path $PSScriptRoot "utils\bootloader\windows"
+if (-not (Test-Path (Join-Path $BootloaderPath 'runw.exe'))) {
+    error "Custom bootloader not found in '$BootloaderPath'"
+}
 
 # Build
 echo "Compiling auto-mcs"
 cd $current
 Copy-Item -Force $spec_file ..\source
 cd ..\source
-pyinstaller $spec_file --clean --log-level INFO 2>&1
+pyinstaller --bootloader-path "$BootloaderPath" $spec_file --clean --log-level INFO 2>&1
 cd $current
 Remove-Item -Force ..\source\$spec_file
 Remove-Item -Force .\dist -ErrorAction SilentlyContinue -Recurse
